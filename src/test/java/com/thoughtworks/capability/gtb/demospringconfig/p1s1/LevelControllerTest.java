77381787148dc19499d846999643ed26beaff2d2
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-21 01:43:59 +0800

    [ruiling] feat: finish p2s3

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/p1s1/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/p1s1/LevelControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,33 +17,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=2")
 public class LevelControllerTest {
-    @Autowired
-    private LevelController levelController;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void should_return_basic_if_levelNumber_be_0_when_less_than_1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 0);
-        mockMvc.perform(get("/level")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("basic"));
-    }
-
-    @Test
-    public void should_return_advanced_if_levelNumber_be_1_when_equal_than_1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 1);
-        mockMvc.perform(get("/level")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("advanced"));
-    }
-
-    @Test
     public void should_return_advanced_if_levelNumber_be_2_when_more_than_1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 2);
         mockMvc.perform(get("/level")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
