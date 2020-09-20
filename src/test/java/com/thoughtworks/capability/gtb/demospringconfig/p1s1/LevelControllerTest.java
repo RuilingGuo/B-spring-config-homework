453c29e8ae58b2aaf5a03a63869a53de4ec032d5
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-21 01:27:36 +0800

    [ruiling] feat: finish p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/p1s1/LevelControllerTest.java
@@ -0,0 +1,51 @@
+package com.thoughtworks.capability.gtb.demospringconfig.p1s1;
+
+import com.thoughtworks.capability.gtb.demospringconfig.LevelController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LevelControllerTest {
+    @Autowired
+    private LevelController levelController;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_basic_if_levelNumber_be_0_when_less_than_1() throws Exception {
+        ReflectionTestUtils.setField(levelController, "levelNumber", 0);
+        mockMvc.perform(get("/level")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("basic"));
+    }
+
+    @Test
+    public void should_return_advanced_if_levelNumber_be_1_when_equal_than_1() throws Exception {
+        ReflectionTestUtils.setField(levelController, "levelNumber", 1);
+        mockMvc.perform(get("/level")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("advanced"));
+    }
+
+    @Test
+    public void should_return_advanced_if_levelNumber_be_0_when_more_than_1() throws Exception {
+        ReflectionTestUtils.setField(levelController, "levelNumber", 2);
+        mockMvc.perform(get("/level")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("advanced"));
+    }
+}
